5792d5ddf208ef53abecc4fc51175f6e252572e8
Erick González <erick.frank@icloud.com>
2018-07-12 23:40:31 -0500

    Initial commit

---
+++ Data_Structure/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Data_Structure
+//
+//  Created by Erick González on 7/12/18.
+//  Copyright © 2018 Erick González. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
